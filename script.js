a3b5a596e44b2bed7b47d310c7e520e983d570ca
samuel kahura <kahurasamuel@gmail.com>
2021-06-02 21:31:56 -0400

    final-commit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
-//Declare a variable to store the searched city
-let city="";
 // variable declaration
+let city="";
 let searchCity = $("#search-city");
 let searchButton = $("#search-button");
 let clearButton = $("#clear-history");
@@ -11,7 +10,7 @@ let currentHumidty= $("#humidity");
 let currentWSpeed=$("#wind-speed");
 let currentUvindex= $("#uv-index");
 let sCity=[];
-// searches the city to see if it exists in the entries from the storage
+
 function find(c){
     for (var i=0; i<sCity.length; i++){
         if(c.toUpperCase()===sCity[i]){
@@ -137,7 +136,7 @@ function invokePastSearch(event){
 
 }
 
-// render function
+// renders function
 function loadlastCity(){
     $("ul").empty();
    let sCity = JSON.parse(localStorage.getItem("cityname"));
